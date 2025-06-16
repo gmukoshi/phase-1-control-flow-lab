30590fee274ffdf4be512509aa17ef702e5904c9
George <mukoshig@gmail.com>
2025-06-16 21:40:58 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(Feet) {
+  if ( Feet <= 400){
+    return 'This one is on me!';
+  }
+  else if(Feet > 400 && Feet <= 2000) {
+    return 'That will be twenty bucks.';
+    }
+    else if (Feet > 2000 && Feet <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+    }
+    else {
+      return 'No can do.';
+    }
+    }
+     
   // Write your code here!
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+  }
